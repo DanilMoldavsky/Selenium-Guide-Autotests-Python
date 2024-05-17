39e085fd1b5ec9554ba9cac773c447499688d516
NoBadVibes79 <krazerog@outlook.com>
2024-05-17 16:54:31 +0300

    step by step

---
+++ section2/lesson1_attr_test.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.ID, "treasure")
+
+    x = x_element.get_attribute("valuex")
+    assert x is not None, "We not find x element"
+    y = calc(x)
+
+    answer = browser.find_element(By.ID, 'answer')
+    answer.send_keys(y)
+    
+    robo_checkbox = browser.find_element(By.ID, 'robotCheckbox')
+    robo_checkbox.click()
+    
+    robots_rule = browser.find_element(By.ID, 'robotsRule')
+    robots_rule.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+    
+
+finally:
+    time.sleep(10)
+    browser.quit()
---
+++ section2/lesson1_attributes.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "https://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    people_radio = browser.find_element(By.ID, "peopleRule")
+
+    people_checked = people_radio.get_attribute("checked")
+    print("value of people radio: ", people_checked)
+    assert people_checked == "true", "People radio is not selected by default"
+    
+
+finally:
+    time.sleep(10)
+    browser.quit()
